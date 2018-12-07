ffde4d279e933e78a80673ed16cf0a90ca99a9af
Ihar Shymanski <shymanski.ihar@gmail.com>
2018-12-07 19:39:02 +0300

    Empty projects

---
+++ DIContainer/DependenciesConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIContainer
+{
+    public class DependenciesConfiguration
+    {
+        private readonly Dictionary<Type, List<Type>> dependencies;
+        DependenciesConfiguration()
+        {
+            dependencies = new Dictionary<Type, List<Type>>();
+        }
+        public void Register<Dep, Impl>()
+        {
+            List<Type> list;
+            if (!dependencies.TryGetValue(typeof(Dep), out list))
+            {
+                list = new List<Type>();
+                dependencies.Add(typeof(Dep), list);
+            }
+            list.Add(typeof(Impl));
+        }
+    }
+}
---
+++ DIContainerTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DIContainerTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DIContainerTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("80975313-c453-4046-b717-4ec5856c5e58")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
